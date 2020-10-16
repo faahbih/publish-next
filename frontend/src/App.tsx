de545fe97a9e414e917f1dcffc4bacc9e42ebf8c
Heitor Carneiro <heitor.carneiro@geoambiente.com.br>
2020-10-15 21:35:06 -0300

    Adding editorconfig and prettier.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,15 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Logo path: {logo}
-        </p>
-        <p>
-          NODE_ENV: {process.env.NODE_ENV}
-        </p>
-        <p>
-          REACT_APP_FOO: {process.env.REACT_APP_FOO}
-        </p>
+        <p>Logo path: {logo}</p>
+        <p>NODE_ENV: {process.env.NODE_ENV}</p>
+        <p>REACT_APP_FOO: {process.env.REACT_APP_FOO}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
