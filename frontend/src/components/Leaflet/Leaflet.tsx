15b9813835d2b647b81a2ab73383057b4ba10064
Heitor Carneiro <heitor.carneiro@geoambiente.com.br>
2020-10-19 20:58:46 -0300

    Adding geojson sample

--- frontend/src/components/Leaflet/Leaflet.tsx
+++ frontend/src/components/Leaflet/Leaflet.tsx
@@ -1,28 +1,54 @@
 import React from 'react';
 
-import { Map, Marker, Popup, TileLayer, ZoomControl } from 'react-leaflet';
-import { LatLng } from 'leaflet';
+import { Map, TileLayer, ZoomControl, GeoJSON } from 'react-leaflet';
+import rawData from 'assets/brazil-forest-code.json';
+
+type MapProperties = {
+  lat: number;
+  lng: number;
+  zoom: number;
+};
 
 function Leaflet() {
-  const position = {
-    lat: -23.17944,
-    lng: -45.88694,
-  } as LatLng;
+  const properties = {
+    lat: -14.35143,
+    lng: -49.01675,
+    zoom: 4,
+  } as MapProperties;
+
+  const geoJSONStyle = () => {
+    return {
+      color: '#1f2021',
+      weight: 1,
+      fillOpacity: 0.5,
+      fillColor: '#fff2af',
+    };
+  };
+
+  const onEachFeature = (feature: any, layer: any) => {
+    const popupContent = `
+      <Popup>
+        <p>Customizable Popups <br />with feature information.</p>
+        <pre>Borough: <br />${feature.properties.name}</pre>
+      </Popup>`;
+    layer.bindPopup(popupContent);
+  };
+
+  const data = rawData as GeoJSON.GeoJsonObject;
 
   return (
-    <Map id="mapId" center={position} zoom={13} zoomControl={false}>
+    <Map
+      id="mapId"
+      center={properties}
+      zoom={properties.zoom}
+      zoomControl={false}
+    >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
       <ZoomControl position="bottomright" />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup.
-          <br />
-          Easily customizable.
-        </Popup>
-      </Marker>
+      <GeoJSON data={data} style={geoJSONStyle} onEachFeature={onEachFeature} />
     </Map>
   );
 }
