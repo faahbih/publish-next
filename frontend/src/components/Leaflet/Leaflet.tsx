e6c5a8b387b28b3cc24723918227e4c75cfb2d1e
Heitor Carneiro <heitor.carneiro@geoambiente.com.br>
2020-10-19 21:25:47 -0300

    Adding chart.js with a sample in Popup.

--- frontend/src/components/Leaflet/Leaflet.tsx
+++ frontend/src/components/Leaflet/Leaflet.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
-import { Map, TileLayer, ZoomControl, GeoJSON } from 'react-leaflet';
+import {
+  Map,
+  TileLayer,
+  ZoomControl,
+  GeoJSON,
+  Marker,
+  Popup,
+} from 'react-leaflet';
 import rawData from 'assets/brazil-forest-code.json';
+
+import { Line } from 'react-chartjs-2';
 
 type MapProperties = {
   lat: number;
@@ -34,7 +43,56 @@ function Leaflet() {
     layer.bindPopup(popupContent);
   };
 
-  const data = rawData as GeoJSON.GeoJsonObject;
+  const spatialData = rawData as GeoJSON.GeoJsonObject;
+
+  // https://stackoverflow.com/questions/59196857/how-to-style-leaflet-popup-width-and-h%D0%B5ight-on-reactjs
+  const chartData = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+      {
+        label: 'My First dataset',
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(75,192,192,1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [65, 59, 80, 81, 56, 55, 40],
+      },
+      {
+        label: 'My Second dataset',
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(75,192,192,1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [15, 109, 1, 23, 78, 9, 200],
+      },
+    ],
+  };
 
   return (
     <Map
@@ -48,7 +106,16 @@ function Leaflet() {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
       <ZoomControl position="bottomright" />
-      <GeoJSON data={data} style={geoJSONStyle} onEachFeature={onEachFeature} />
+      <GeoJSON
+        data={spatialData}
+        style={geoJSONStyle}
+        onEachFeature={onEachFeature}
+      />
+      <Marker position={properties}>
+        <Popup minWidth={400}>
+          <Line data={chartData} />
+        </Popup>
+      </Marker>
     </Map>
   );
 }
