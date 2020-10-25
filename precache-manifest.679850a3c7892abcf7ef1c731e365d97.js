971351c9ac5b7cae27e3f6660c26b7f2d8122c25
Heitor Carneiro <heitor.carneiro@geoambiente.com.br>
2020-10-25 18:56:15 -0300

    Updates

---
+++ precache-manifest.679850a3c7892abcf7ef1c731e365d97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9976aca5ef409b60ef1de8cd3dda120f",
+    "url": "/publish-next/index.html"
+  },
+  {
+    "revision": "07d48f67e0b7155e6a9a",
+    "url": "/publish-next/static/css/main.5b12a4f8.chunk.css"
+  },
+  {
+    "revision": "04fbbdf145c564de42f8",
+    "url": "/publish-next/static/js/2.4a6ac0de.chunk.js"
+  },
+  {
+    "revision": "d810cc6ba41a3292177fd0ac7570f5b1",
+    "url": "/publish-next/static/js/2.4a6ac0de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07d48f67e0b7155e6a9a",
+    "url": "/publish-next/static/js/main.b93c0921.chunk.js"
+  },
+  {
+    "revision": "6bd60a14ba93da98f380",
+    "url": "/publish-next/static/js/runtime-main.c10acf74.js"
+  }
+]);
