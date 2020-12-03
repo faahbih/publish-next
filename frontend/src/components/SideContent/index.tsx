57184427139c54c877873f229b89035a7b6aca49
Fabiana <fabiana.oliveira@guichevirtual.com.br>
2020-12-03 08:58:34 -0300

    Update SideContent

--- frontend/src/components/SideContent/index.tsx
+++ frontend/src/components/SideContent/index.tsx
@@ -7,10 +7,6 @@ import {
   AccordionDetails,
   AccordionSummary,
   Chip,
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
   Tooltip,
   Typography,
 } from '@material-ui/core';
@@ -29,13 +25,17 @@ const useStyles = makeStyles((theme: Theme) =>
       padding: '86px 16px',
     },
     title: {
-      fontSize: 14,
+      fontSize: 12,
     },
     main: {
       width: '100%',
     },
     customAccordion: {
       boxShadow: 'none',
+      margin: '4px 0',
+    },
+    customAccordionSummary: {
+      padding: '0px 8px',
     },
     customAccordionDetails: {
       flexWrap: 'wrap',
@@ -46,7 +46,7 @@ const useStyles = makeStyles((theme: Theme) =>
       fontWeight: theme.typography.fontWeightMedium,
     },
     chipSpacing: {
-      margin: 8,
+      margin: '2px 4px',
     },
     formControl: {
       margin: theme.spacing(1),
@@ -56,21 +56,29 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
-const longText = `
-Aliquam eget finibus ante, non facilisis lectus. Sed vitae dignissim est, vel aliquam tellus.
-Praesent non nunc mollis, fermentum neque at, semper arcu.
-Nullam eget est sed sem iaculis gravida eget vitae justo.
+const textFC = `
+Attempt to capture the future impacts of all key provisions of a
+rigorously enforced Brazil's Forest Code
 `;
+
+const textNoFC = `Allow both legal and illegal deforestation at all times, which is
+driven by the demand for agricultural commodities, and does not include
+any policy restrictions.`;
+
+const textCrop = `Planted areas with the following crops:
+barley, dry beans, cassava, corn, cotton, groundnut, palm oil, potato,
+rice, sorghum, soybeans, sugarcane, sweet potato, wheat.`;
+
+const textGrass = `Pasture areas used for livestock ranching.`;
+const textNatV = `Native vegetation including rainforest and savannas.`;
+const textNatVC = `Native vegetation loss due to pasture and
+cropland expansions.`;
+const textLU = `Large Unit (200x200Km at Equator)`;
 
 export default function Legend() {
   const classes = useStyles();
   const handleClick = () => {
     console.info('You clicked the Chip.');
-  };
-
-  const [age, setAge] = React.useState('');
-  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
-    setAge(event.target.value as string);
   };
 
   return (
@@ -87,45 +95,15 @@ export default function Legend() {
         <div className={classes.main}>
           <Accordion className={classes.customAccordion}>
             <AccordionSummary
+              className={classes.customAccordionSummary}
               expandIcon={<ExpandMoreIcon />}
               aria-controls="panel1a-content"
               id="panel1a-header"
             >
-              <Typography className={classes.heading}>Borders</Typography>
-            </AccordionSummary>
-            <AccordionDetails className={classes.customAccordionDetails}>
-              <Tooltip title={longText} arrow>
-                <Chip
-                  label="Brazil"
-                  onClick={handleClick}
-                  variant="outlined"
-                  className={classes.chipSpacing}
-                />
-              </Tooltip>
-              <Chip
-                label="Biomes"
-                onClick={handleClick}
-                variant="outlined"
-                style={{
-                  backgroundColor: '#757575',
-                  color: '#fff',
-                  border: 'none',
-                }}
-                className={classes.chipSpacing}
-              />
-            </AccordionDetails>
-          </Accordion>
-
-          <Accordion className={classes.customAccordion}>
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel2a-content"
-              id="panel2a-header"
-            >
               <Typography className={classes.heading}>Scenarios</Typography>
             </AccordionSummary>
             <AccordionDetails className={classes.customAccordionDetails}>
-              <Tooltip title={longText} arrow>
+              <Tooltip title={textFC} arrow>
                 <Chip
                   label="FC"
                   onClick={handleClick}
@@ -133,30 +111,44 @@ export default function Legend() {
                   className={classes.chipSpacing}
                 />
               </Tooltip>
-              <Chip
-                label="NoFC"
-                onClick={handleClick}
-                variant="outlined"
-                className={classes.chipSpacing}
-                style={{
-                  backgroundColor: '#EB4B35',
-                  color: '#fff',
-                  border: 'none',
-                }}
-              />
-
-              <Typography
-                variant="caption"
-                display="block"
-                gutterBottom
-                style={{ marginLeft: '14px', marginTop: '16px' }}
-              >
-                Select the background
-              </Typography>
-
-              <div>
+              <Tooltip title={textNoFC} arrow>
                 <Chip
-                  label="CR"
+                  label="NoFC"
+                  onClick={handleClick}
+                  variant="outlined"
+                  style={{
+                    backgroundColor: '#EB4B35',
+                    color: '#fff',
+                    border: 'none',
+                  }}
+                  className={classes.chipSpacing}
+                />
+              </Tooltip>
+            </AccordionDetails>
+          </Accordion>
+
+          <Accordion className={classes.customAccordion}>
+            <AccordionSummary
+              className={classes.customAccordionSummary}
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2a-content"
+              id="panel2a-header"
+            >
+              <Typography className={classes.heading}>Attributes</Typography>
+            </AccordionSummary>
+            <AccordionDetails className={classes.customAccordionDetails}>
+              <Tooltip title={textFC} arrow>
+                <Chip
+                  label="Soybean"
+                  onClick={handleClick}
+                  variant="outlined"
+                  className={classes.chipSpacing}
+                />
+              </Tooltip>
+
+              <Tooltip title={textCrop} arrow>
+                <Chip
+                  label="Cropland"
                   onClick={handleClick}
                   variant="outlined"
                   className={classes.chipSpacing}
@@ -166,72 +158,95 @@ export default function Legend() {
                     border: 'none',
                   }}
                 />
+              </Tooltip>
+
+              <Tooltip title={textGrass} arrow>
                 <Chip
-                  label="SimU"
+                  label="Grassland"
                   onClick={handleClick}
                   variant="outlined"
                   className={classes.chipSpacing}
                 />
+              </Tooltip>
+
+              <Tooltip title={textNatV} arrow>
+                <Chip
+                  label="Native Vegetation"
+                  onClick={handleClick}
+                  variant="outlined"
+                  className={classes.chipSpacing}
+                />
+              </Tooltip>
+
+              <Tooltip title={textNatVC} arrow>
+                <Chip
+                  label="Native Vegetation Conversion"
+                  onClick={handleClick}
+                  variant="outlined"
+                  className={classes.chipSpacing}
+                />
+              </Tooltip>
+            </AccordionDetails>
+          </Accordion>
+
+          <Accordion className={classes.customAccordion}>
+            <AccordionSummary
+              className={classes.customAccordionSummary}
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel3a-content"
+              id="panel3a-header"
+            >
+              <Typography className={classes.heading}>Borders</Typography>
+            </AccordionSummary>
+            <AccordionDetails className={classes.customAccordionDetails}>
+              <Chip
+                label="Brazil"
+                onClick={handleClick}
+                variant="outlined"
+                className={classes.chipSpacing}
+              />
+              <Chip
+                label="Biomes"
+                onClick={handleClick}
+                variant="outlined"
+                className={classes.chipSpacing}
+                style={{
+                  backgroundColor: '#757575',
+                  color: '#fff',
+                  border: 'none',
+                }}
+              />
+            </AccordionDetails>
+          </Accordion>
+
+          <Accordion className={classes.customAccordion}>
+            <AccordionSummary
+              className={classes.customAccordionSummary}
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel3a-content"
+              id="panel3a-header"
+            >
+              <Typography className={classes.heading}>Background</Typography>
+            </AccordionSummary>
+            <AccordionDetails className={classes.customAccordionDetails}>
+              <Tooltip title="ColRow (50x50km at Equator)" arrow>
+                <Chip
+                  label="CR"
+                  onClick={handleClick}
+                  variant="outlined"
+                  className={classes.chipSpacing}
+                />
+              </Tooltip>
+              <Tooltip title={textLU} arrow>
                 <Chip
                   label="LU"
                   onClick={handleClick}
                   variant="outlined"
                   className={classes.chipSpacing}
                 />
-              </div>
-
-              <Typography
-                variant="caption"
-                display="block"
-                gutterBottom
-                style={{ marginLeft: '14px', marginTop: '16px' }}
-              >
-                Select an Attribute
-              </Typography>
-
-              <FormControl className={classes.formControl}>
-                <InputLabel id="demo-simple-select-label">Attribute</InputLabel>
-                <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={age}
-                  onChange={handleChange}
-                >
-                  <MenuItem value={10}>Soybean</MenuItem>
-                  <MenuItem value={20}>Cropland</MenuItem>
-                  <MenuItem value={30}>Grassland</MenuItem>
-                  <MenuItem value={30}>Native Vegetation</MenuItem>
-                  <MenuItem value={30}>Native Vegetation Conversion</MenuItem>
-                </Select>
-              </FormControl>
+              </Tooltip>
             </AccordionDetails>
           </Accordion>
-
-          {/* <Accordion className={classes.customAccordion}>
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel3a-content"
-              id="panel3a-header"
-            >
-              <Typography className={classes.heading}>
-                Social Classes
-              </Typography>
-            </AccordionSummary>
-            <AccordionDetails className={classes.customAccordionDetails}>
-              <Chip
-                label="Social Classes 2025"
-                onClick={handleClick}
-                variant="outlined"
-                className={classes.chipSpacing}
-              />
-              <Chip
-                label="Social Classes 2010"
-                onClick={handleClick}
-                variant="outlined"
-                className={classes.chipSpacing}
-              />
-            </AccordionDetails>
-          </Accordion> */}
         </div>
       </CardContent>
     </Card>
